15db9d6e7b2787b1a471c7f54a81324027c8b4bf
huangguan <253354068@qq.com>
2020-09-16 18:25:16 +0800

    mybatis-plus

--- src/main/java/com/example/Entity/Countries.java
+++ src/main/java/com/example/Entity/Countries.java
@@ -1,12 +1,30 @@
 package com.example.Entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
-@Data
-public class Countries {
+import java.io.Serializable;
 
-  private String countryId;
-  private String countryName;
-  private long regionId;
+/**
+ * @Description  
+ * @Author  HG
+ * @Date 2020-09-16 
+ */
+
+@Data
+@TableName( value ="countries" )
+public class Countries  implements Serializable {
+
+	private static final long serialVersionUID =  6272380669670240504L;
+
+   	@TableField(value = "country_id" )
+	private String countryId;
+
+   	@TableField(value = "country_name" )
+	private String countryName;
+
+   	@TableField(value = "region_id" )
+	private Long regionId;
 
 }
