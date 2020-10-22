d079060a6fa79751b6900238fa32800c07c93a3a
黄冠 <253354068@qq.com>
2020-10-22 10:31:15 +0800

    master

--- src/main/java/com/example/Entity/Countries.java
+++ src/main/java/com/example/Entity/Countries.java
@@ -1,7 +1,6 @@
 package com.example.Entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
